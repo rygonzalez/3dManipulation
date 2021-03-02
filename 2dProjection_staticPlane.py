2ce67da218893ebe2b5c7690b7e2ebeef64c0823
rgonzalez <gonz.rya@gmail.com>
2021-03-02 13:13:25 -0500

    Distance storing in projections
    Added distance values to point projection storage. May be useful down road

--- 2dProjection_staticPlane.py
+++ 2dProjection_staticPlane.py
@@ -22,11 +22,14 @@ def YZplane(): # some of this is hard-coded in - will need to play with more
 # plane projection method for body composing of a 3xN array of XYZ coordinates
 def projectBody(body, origin, normal):
     projection = np.zeros(body.shape) # initialize array
+    distances = np.zeros([projection.shape[0], 1]) # also for distances to plane
     for i in range(body.shape[0]): # calculate projection
         point = body[i]
         v = point - origin
         dist = v[0]*normal[0] + v[1]*normal[1] + v[2]*normal[2]
-        projection[i] = point - dist*normal
+        projection[i] = point - dist*normal # projection of point onto plane
+        distances[i] = dist # distance from point to plane
+    projection = np.hstack((projection, distances))
     return projection
 
 # make a 3xN XYZ coord array for a line
@@ -50,31 +53,34 @@ def makeSpiral(spiralOrigin):
     spiral = spiral[:] + spiralOrigin # translate to spiral's origin
     return spiral
 
-# generate plane stuff
-X, Y, Z, origin, norm = YZplane()
-
-# generate the body
-# body = makeLine(10, 5, -20, lineOrigin=np.array([30, 0, 0])) # for line
-body = makeSpiral(spiralOrigin=np.array([30, 0, 0])) # for spiral
-
-# generate a project (can use line or spiral as of now)
-projection = projectBody(body, origin, norm)
-
-#plotting plane
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-plt.tight_layout()
-ax.plot_surface(X, Y, Z, alpha = 0.3)
-
-# plotting originals & projections
-ax.plot(body[:,0], body[:,1], body[:,2])
-plt.plot(projection[:,0], projection[:,1], projection[:,2], 'b--')
-plt.plot(origin[0], origin[1], origin[2], 'r+')
-ax.legend(['Original','Projected', 'Plane Origin'])
-
-# uncomment/alter to change viewing angle
-# ax.view_init(elev=0,azim=0) # head-on to plane
-# ax.view_init(elev=0,azim=-90) # parallel to plane
-# ax.view_init(elev=90,azim=0) # above plane
-
-plt.show() # display figures at end
+# script main here, for easily collabsable code
+def main():
+    # generate plane stuff
+    X, Y, Z, origin, norm = YZplane()
+    
+    # generate the body
+    # body = makeLine(10, 5, -20, lineOrigin=np.array([30, 0, 0])) # for line
+    body = makeSpiral(spiralOrigin=np.array([30, 0, 0])) # for spiral
+    
+    # generate a project (can use line or spiral as of now)
+    projection = projectBody(body, origin, norm)
+    
+    #plotting plane
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    plt.tight_layout()
+    ax.plot_surface(X, Y, Z, alpha = 0.3)
+    
+    # plotting originals & projections
+    ax.plot(body[:,0], body[:,1], body[:,2])
+    plt.plot(projection[:,0], projection[:,1], projection[:,2], 'b--')
+    plt.plot(origin[0], origin[1], origin[2], 'r+')
+    ax.legend(['Original','Projected', 'Plane Origin'])
+    
+    # uncomment/alter to change viewing angle
+    # ax.view_init(elev=0,azim=0) # head-on to plane
+    # ax.view_init(elev=0,azim=-90) # parallel to plane
+    # ax.view_init(elev=90,azim=0) # above plane
+    
+    plt.show() # display figures at end
+main()
